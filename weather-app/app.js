e4d79c6d8c03abfccfc399c18d319e3e3df340ad
Diego Keller <keller.diego@gmail.com>
2017-02-01 17:57:17 -0200

    .

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,34 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+console.log(argv);
+
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=Rua%20Frei%20Jose,%20311,%20Blumenau',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body)=>{
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  if(error){
+    console.log('Unable to connect to Google servers.');
+  } else if(body.status === 'ZERO_RESULTS'){
+    console.log('Address not found.');
+  } else if(body.status === 'OK') {
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  }
 });
