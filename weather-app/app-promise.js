3aac136d3b8597fbe4f523d065bd9bd8217438ca
Diego Keller <keller.diego@gmail.com>
2017-02-02 17:19:00 -0200

    Weather app async.

---
+++ weather-app/app-promise.js
@@ -0,0 +1,41 @@
+
+const yargs = require('yargs');
+const axios = require('axios');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
+var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+
+axios.get(geocodeUrl).then((response) => {
+  if (response.data.status === 'ZERO_RESULTS') {
+    throw new Error('Address not found.');
+  }
+
+  var latitude = response.data.results[0].geometry.location.lat;
+  var longitude = response.data.results[0].geometry.location.lng;
+  var weatherUrl = `https://api.darksky.net/forecast/2a7af0e8d3e3ff8e55b927b5e628c3c2/${latitude},${longitude}`;
+  console.log(response.data.results[0].formatted_address);
+  return axios.get(weatherUrl);
+}).then((response) => {
+  var temperature = response.data.currently.temperature;
+  var apparentTemperature = response.data.currently.apparentTemperature;
+  console.log(`It's currently ${temperature}. It feals like ${apparentTemperature}`);
+}).catch((e) => {
+  if (e.code === 'ENOTFOUND') {
+    console.log('Unable to connect to API server.');
+  } else {
+    console.log(e.message);
+  }
+});
