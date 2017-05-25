0fd1b554b647f1db2b2240bbbca8a487ef551bcc
Diego Keller <keller.diego@gmail.com>
2017-05-25 08:45:40 -0300

    GET /todo/:id route todo by id

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -5,6 +5,8 @@ var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
+const {ObjectID} = require('mongodb');
+
 var app = express();
 
 app.use(bodyParser.json());
@@ -27,6 +29,28 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+
+    if(!todo){
+        return res.status(404).send();
+    }
+
+    return res.status(200).send({todo});
+
+  }).catch((e) => {
+      return res.status(400).send();
+  });
+
 });
 
 app.listen(3000, () => {
